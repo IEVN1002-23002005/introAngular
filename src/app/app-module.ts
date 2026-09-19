9c38e3f3afbdc5a9b3a6d406d63ddac73feeb61b
IEVN1002-23002005 <82817@alumnos.utleon.edu.mx>
2026-09-19 03:53:20 -0600

    NGMODEL 18/09/2026

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,12 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { HeroesList } from './heroes/heroes-list/heroes-list';
 
 @NgModule({
-  declarations: [App, HeroesList],
-  imports: [BrowserModule],
-  providers: [],
-  bootstrap: [App],
+  declarations: [
+    App,
+    HeroesList
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/heroes/heroes-list/heroes-list.ts
+++ src/app/heroes/heroes-list/heroes-list.ts
@@ -6,4 +6,47 @@ import { Component } from '@angular/core';
   styleUrl: './heroes-list.css',
   templateUrl: './heroes-list.html',
 })
-export class HeroesList {}
+export class HeroesList {
+
+  listFilter: string = '';
+  muestraImage: boolean = true;
+  imageWidth: number = 80;
+  imageMargin: number = 2;
+
+  heroes: any[] = [
+    {
+      imagen: 'https://dragonball-api.com/characters/gohan.webp',
+      nombre: 'Gohan',
+      descripcion: 'Hijo de Goku y poderoso guerrero',
+      race: 'Saiyan',
+      ki: 40000000
+    },
+    {
+      imagen: 'https://dragonball-api.com/characters/Freezer.webp',
+      nombre: 'Freezer',
+      descripcion: 'Emperador del universo',
+      race: 'Frieza Race',
+      ki: 530000
+    },
+    {
+      imagen: "https://dragonball-api.com/characters/Krilin_Universo7.webp",
+      nombre: 'Krilin',
+      descripcion: 'Guerrero humano y amigo de Goku',
+      race: 'Human',
+      ki: 75000
+    },
+    {
+      imagen: 'https://dragonball-api.com/characters/bulma.webp',
+      nombre: 'Bulma',
+      descripcion: 'Científica de Capsule Corporation',
+      race: 'Human',
+      ki: 5
+    }
+  ];
+
+  showImage(): void {
+    this.muestraImage = !this.muestraImage;
+  }
+
+}
+
